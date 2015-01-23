ce65e68c9c4a367cca4ec668c2a06829668c5581
Andrzej Zaborowski <andrew.zaborowski@intel.com>
2015-01-23 03:53:50 +0100

    Slave-Select pin must be an output before SPI init.
    Otherwise anytime the SPI sees a high level on SS it'll change our own
    mode to slave and effectively undo our SPI configuration, breaking
    later nRF24 init.

--- optiboot/bootloaders/optiboot/spi.h
+++ optiboot/bootloaders/optiboot/spi.h
@@ -8,6 +8,7 @@
 #define SCK_PIN		(1 << 5)
 #define MISO_PIN	(1 << 4)
 #define MOSI_PIN	(1 << 3)
+#define SS_PIN		(1 << 2)
 
 static void spi_mode(uint8_t mode) {
 	/* Enable SPI master with configuration byte specified */
@@ -22,6 +23,8 @@ static void spi_init(void) {
 	/* Initialize the SPI pins: SCK & MOSI as outputs, MISO as input */
 	SPI_DDR |= SCK_PIN | MOSI_PIN;
 	SPI_DDR &= ~MISO_PIN;
+	/* Must also pre-set SS to output, otherwise it's going to bite us */
+	SPI_DDR |= SS_PIN;
 
 	/* Enable SPI Master, MSB, SPI mode 0, FOSC/4 */
 	spi_mode(0);
